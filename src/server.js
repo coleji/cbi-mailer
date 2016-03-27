a17bbfbb301b64341fe9184abaa5653ecec6cef4
Jonathan <coleji@bc.edu>
2016-03-27 13:43:46 -0400

    test parsing post body

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 import Express from 'express';
+import bodyParser from 'body-parser';
 
 const app = new Express();
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 app.post('/email', function (req, res) {
   console.log("got a post request!")
-  res.send('hello from yawl!');
+  console.log("testParam is " + req.body.testParam);
+  res.send('hello from yawl!  You send me ' + req.body.testParam);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
