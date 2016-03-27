fb3955e42331c66ce7af8a5ac2ac925b570dce2f
Jonathan <coleji@bc.edu>
2016-03-27 13:24:50 -0400

    about to smoketest post connection

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import Express from 'express';
 
 const app = new Express();
 
+app.post('/email', function (req, res) {
+  console.log("got a post request!")
+  res.send('hello from yawl!');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
