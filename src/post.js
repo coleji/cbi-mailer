fb3452aa12292dfade6806b265bee28cb6b2d8e3
Jonathan <coleji@bc.edu>
2016-03-28 19:27:07 -0400

    TODOs for various components

---
+++ src/post.js
@@ -0,0 +1,39 @@
+const REQUIRED_PARAMS = {
+  trackingId,
+  toAddr,
+  fromAddr,
+  replyToAddr,
+  subject,
+  plainBody
+};
+
+const OPTIONAL_PARAMS = {
+  htmlBody,
+  miscHeaders,
+  ccAddr,
+  bccAddr
+}
+
+const respondFailure = function() {
+  // TODO: write to log file
+  // TODO: repond with failure code
+};
+
+export default function(req, res) {
+  for (var param in REQUIRED_PARAMS) {
+    if (!String(req.body.param)) {
+
+    }
+  }
+
+  // TODO: check message digest
+
+  // TODO: check if trackingid is already in either the pending or sent tables
+
+  // TODO: save to db
+
+  // TODO: send
+
+  // TODO: write tracking id to sent table, purge data from pending table
+
+}
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
+import postProcessor from './post';
 
 const app = new Express();
 
@@ -7,11 +8,11 @@ app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/email', function (req, res) {
-  console.log("got a post request!")
-  console.log("testParam is " + req.body.testParam);
-  res.send('hello from yawl!  You send me ' + req.body.testParam);
+  postProcessor(req, res);
 });
 
 app.listen(80, function () {
   console.log('Example app listening on port 80!');
 });
+
+// TODO: check for messages in our db leftover from some previous session
