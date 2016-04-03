1179233ba6f420607cba2cc2a6f3805f42edf0e5
root <root@community-boating-promo.org>
2016-04-03 12:55:45 -0400

    smoketested smtp connection (tls disabled)

--- src/mailer.js
+++ src/mailer.js
@@ -1,13 +1,20 @@
 import nodemailer from 'nodemailer';
+import ssl from 'ssl-root-cas/latest';
+
+ssl.inject();
+ssl.addFile('/etc/ssl/digicert/DigiCertCA.crt');
+ssl.addFile('/etc/ssl/digicert/TrustedRoot.crt');
 
 export default function() {
 
   var smtpConfig = {
       host: 'localhost',
       port: 25,
-      auth: {
-          user: 'donotreply@community-boating-promo.org'  // TODO: pull out into config
-      }
+      useTLS: false,
+      ignoreTLS: true
+//      auth: {
+//          user: 'donotreply@community-boating-promo.org'  // TODO: pull out into config
+//      }
   };
 
   const transporter = nodemailer.createTransport(smtpConfig);
@@ -19,5 +26,7 @@ export default function() {
           console.log('Server is ready to take our messages');
      }
   });
+
+  console.log('made it here');
 
 };
--- src/server.js
+++ src/server.js
@@ -1,24 +1,22 @@
 import Express from 'express';
 import bodyParser from 'body-parser';
-import postProcessor from './post';
+//import postProcessor from './post';
 import mailer from './mailer.js';
 
 mailer();
 
-/*
 
 const app = new Express();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
+/*
 app.post('/email', function (req, res) {
   postProcessor(req, res);
 });
-
+*/
 app.listen(80, function () {
   console.log('Example app listening on port 80!');
 });
 
-*/
 // TODO: check for messages in our db leftover from some previous session
