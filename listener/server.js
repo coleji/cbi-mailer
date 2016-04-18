6328bd08458c5f0fe0da63723d2673574cfc1f0a
Jonathan <coleji@bc.edu>
2016-04-18 07:54:39 -0400

    added server back to listener

--- listener/server.js
+++ listener/server.js
@@ -1,8 +1,7 @@
 import fs from 'fs';
-/*import express from 'express';
+import express from 'express';
 import https from 'https';
 import bodyParser from 'body-parser';
-*/
 import ini from 'ini';
 
 import postProcessor from './post';
@@ -13,6 +12,7 @@ const PRIVATE_CONFIG = ini.parse(fs.readFileSync('./config/private.ini', 'utf-8'
 // create connection pool
 initDB(PRIVATE_CONFIG.database);
 
+/*
 let req = {
 	body: PRIVATE_CONFIG.test_email,
 	privateConfig: PRIVATE_CONFIG,
@@ -25,34 +25,32 @@ let res = {
 };
 
 postProcessor(req, res);
+*/
+var app = express();
 
-//var app = express();
-/*
 var httpsOptions = {
-  key: fs.readFileSync(''),
-  cert: fs.readFileSync('')
+  key: fs.readFileSync(PRIVATE_CONFIG.ssl.key_path),
+  cert: fs.readFileSync(PRIVATE_CONFIG.ssl.cert_path)
 };
 
 https.createServer(httpsOptions, app).listen(443, function () {
   console.log('https listening on port 443!');
 });
 
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+app.post('/email', function(req, res) {
+  postProcessor(req, res)
+});
+
 app.all('*', function(req, res) {
   console.log('https: ' + req.url);
-  res.send('hello encrypted!!');
+  res.redirect('http://www.community-boating.org');
 });
-*/
 
-/*
 // redirect all unencrypted traffic to main website
 var http = express();
-
-http.use(bodyParser.json()); // support json encoded bodies
-http.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-http.post('/email', (req, res) => {
-	postProcessor(req, res);
-});
 
 http.all('*', function(req, res) {
   return res.redirect("http://www.community-boating.org");
@@ -60,6 +58,4 @@ http.all('*', function(req, res) {
 
 http.listen(80, function () {
   console.log('http listening on port 80!');
-});*/
-
-// TODO: check for messages in our db leftover from some previous session
+});
