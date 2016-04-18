a02a622766de03269cab3fd89b43304018f05739
Jonathan <coleji@bc.edu>
2016-04-18 09:06:37 -0400

    fixed spacing

--- listener/server.js
+++ listener/server.js
@@ -29,33 +29,33 @@ postProcessor(req, res);
 var app = express();
 
 var httpsOptions = {
-  key: fs.readFileSync(PRIVATE_CONFIG.server.key_path),
-  cert: fs.readFileSync(PRIVATE_CONFIG.server.cert_path)
+	key: fs.readFileSync(PRIVATE_CONFIG.server.key_path),
+	cert: fs.readFileSync(PRIVATE_CONFIG.server.cert_path)
 };
 
 https.createServer(httpsOptions, app).listen(443, function () {
-  console.log('https listening on port 443!');
+	console.log('https listening on port 443!');
 });
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.post('/email', function(req, res) {
-  postProcessor(req, res)
+	postProcessor(req, res)
 });
 
 app.all('*', function(req, res) {
-  console.log('https: ' + req.url);
-  res.redirect('http://www.community-boating.org');
+	console.log('https: ' + req.url);
+	res.redirect('http://www.community-boating.org');
 });
 
 // redirect all unencrypted traffic to main website
 var http = express();
 
 http.all('*', function(req, res) {
-  return res.redirect("http://www.community-boating.org");
+	return res.redirect("http://www.community-boating.org");
 });
 
 http.listen(80, function () {
-  console.log('http listening on port 80!');
+	console.log('http listening on port 80!');
 });
